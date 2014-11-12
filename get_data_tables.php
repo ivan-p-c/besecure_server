79c309235301d938099d14db2b9803a7d971b9d5
ivanp <i.palomares-carrascosa@ulster.ac.uk>
2014-11-12 09:41:23 +0000

    Query in get_data_tables.php modified to take into account geography level

--- get_data_tables.php
+++ get_data_tables.php
@@ -4,6 +4,7 @@
 // Grab the posted data from the AJAX POST method ($.post)
 $category = $_POST['category'];
 $schema = $_POST['cs_area'];
+$geo_level = $_POST['geo_level'];
  
 $dbconn = pg_connect("host=localhost port=5432 dbname=besecure_data user=postgres password=postgres")
 or die('Could not connect: ' . pg_last_error());
@@ -11,9 +12,13 @@ or die('Could not connect: ' . pg_last_error());
 $pre_query = 'SELECT id FROM metadata.categories WHERE name = \''.$category.'\'';	
 $pre_result = pg_query($pre_query) or die('Query failed: ' . pg_last_error());
 $pre_out=pg_fetch_assoc($pre_result);
+
+$geo_level_query = 'SELECT id FROM metadata.geography_levels WHERE name = \''.$geo_level.'\'';
+$geo_level_result = pg_query($geo_level_query) or die('Query failed: ' . pg_last_error());
+$geo_level_id = pg_fetch_assoc($geo_level_result);
 	
 // Performing SQL query
-$query = 'SELECT tablename,name_shown FROM '.$schema.'.tables_list_'.$schema.' WHERE category_id = \''.$pre_out['id'].'\' ORDER BY name_shown ASC';
+$query = 'SELECT tablename,name_shown FROM '.$schema.'.tables_list_'.$schema.' WHERE category_id = \''.$pre_out['id'].'\' AND geography_level_id = \''.$geo_level_id['id'].'\' ORDER BY name_shown ASC';
 $result = pg_query($query) or die('Query failed: ' . pg_last_error());
 
 //Fetch all the result in an array
